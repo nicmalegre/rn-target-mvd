327d08f88b22d1e9aaccee5032e57c3881cdb5c6
Nicolás Alegre <nikoalegre@hotmail.com>
2022-05-05 10:35:07 -0300

    Add right arrow in ProfileScreen and onPressIconRight

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -1,3 +1,4 @@
 export const PROFILE_ICON = () => require('assets/icons/Profile/Profile.png');
 export const CHAT_ICON = () => require('assets/icons/Chat/Chat.png');
 export const BACK_ARROW_ICON = () => require('assets/icons/BackArrow/BackArrow.png');
+export const RIGHT_ARROW_ICON = () => require('assets/icons/RightArrow/RightArrow.png');
--- src/screens/ProfileScreen/index.js
+++ src/screens/ProfileScreen/index.js
@@ -1,15 +1,18 @@
 import React, { memo, useCallback } from 'react';
 import { Text, View, TouchableOpacity, Image } from 'react-native';
 import { useDispatch } from 'react-redux';
+import { useNavigation } from '@react-navigation/native';
 
 import UpdateProfileForm from 'components/UpdateProfileForm';
-import { SIGN_UP_SCREEN } from 'constants/screens';
+import { MAIN_SCREEN, SIGN_UP_SCREEN } from 'constants/screens';
 import { logout, updateUser } from 'actions/userActions';
 import strings from 'localization';
 import Container from 'components/common/Container';
+import { RIGHT_ARROW_ICON } from 'constants/icons';
 import styles from './styles';
 
 const ProfileScreen = memo(() => {
+  const navigation = useNavigation();
   const dispatch = useDispatch();
   const updateUserRequest = useCallback(user => dispatch(updateUser(user)), [dispatch]);
   const logoutRequest = useCallback(() => dispatch(logout()), [dispatch]);
@@ -18,8 +21,8 @@ const ProfileScreen = memo(() => {
     <Container
       headerProps={{
         title: strings.PROFILE_SCREEN.title,
-        // TO DO: open chat on press icon right
-        onPressIconRight: () => {},
+        imageRight: RIGHT_ARROW_ICON(),
+        onPressIconRight: () => navigation.navigate(MAIN_SCREEN),
       }}>
       <View style={styles.container} testID={SIGN_UP_SCREEN}>
         <Image source={require('assets/images/logos/profile-image.png')} style={styles.image} />

06394d9b7a83484e3e52d222981d7334d10e6217
Nicolás Alegre <nikoalegre@hotmail.com>
2022-05-11 13:23:50 -0300

    Fix interceptor to avoid header error in GET request

--- src/services/topicService.js
+++ src/services/topicService.js
@@ -2,8 +2,7 @@ import httpClient from 'httpClient';
 
 class TopicService {
   getTopics() {
-    // FYI: 'data:null' is to avoid header error in GET request
-    return httpClient.get('/topics', { data: null });
+    return httpClient.get('/topics');
   }
 }
 

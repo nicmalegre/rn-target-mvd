522e31a05e6bc679f88aa3551792ac26ab4d41e7
Nicolás Alegre <nikoalegre@hotmail.com>
2022-05-13 19:34:23 -0300

    Add TopicIcon component

--- src/components/TargetMarker/index.js
+++ src/components/TargetMarker/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { View, Image } from 'react-native';
 import { useSelector } from 'react-redux';
 
 import { Marker } from 'react-native-maps';
 import strings from 'localization';
-import { BACKGROUND_TARGET_MARKER } from 'constants/colors';
 import { number } from 'prop-types';
+import TopicIcon from 'components/TopicIcon';
 
 const TargetMarker = ({ latitude, longitude, topicId }) => {
   const { topics } = useSelector(({ topics }) => ({
@@ -24,18 +23,7 @@ const TargetMarker = ({ latitude, longitude, topicId }) => {
         longitude,
       }}
       title={strings.MAIN_SCREEN.markerTitle}>
-      <View
-        style={{
-          backgroundColor: BACKGROUND_TARGET_MARKER,
-          height: 55,
-          width: 55,
-          borderRadius: 50,
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <Image source={{ uri: topic.icon }} style={{ height: 25, width: 25 }} />
-      </View>
+      <TopicIcon uri={topic.icon} />
     </Marker>
   );
 };
